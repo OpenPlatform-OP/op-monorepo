9aa6e0da6c073c52bd1bd99ee8987d1ed2b38e86
Leo Liao <honglin0822@outlook.com>
2023-04-03 02:34:09 +0800

    seperate test from pages

--- apps/web-app/jest/setupRouter.ts
+++ apps/web-app/jest/setupRouter.ts
@@ -3,3 +3,5 @@ jest.mock('next/router', () => ({
     push: () => null,
   })),
 }));
+
+export {};
--- apps/web-app/specs/index.test.tsx
+++ apps/web-app/specs/index.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import getDiscordOAuthConfig from '@/utils/discord';
-import Index from '..';
+import Index from '../pages';
 
 describe('pages > index', () => {
   it('should render feature menu', () => {
