8dd078805011e498ce1c55e00c526452773b1eb3
Eric Lee <sdusteric@gmail.com>
2023-04-30 11:39:56 -0700

    ci: correct coverage report

--- apps/web-app/components/WebButton/WebButton.tsx
+++
@@ -1,6 +0,0 @@
-import { Button } from '@mui/material';
-import { FC } from 'react';
-
-export const WebButton: FC = () => {
-  return <Button id="web-button">Click</Button>;
-};
--- libs/base-ui/src/lib/TestButton/TestButton.tsx
+++
@@ -1,6 +0,0 @@
-import { Button } from '@mui/material';
-import { FC } from 'react';
-
-export const TestButton: FC = () => {
-  return <Button id="test-button">Click</Button>;
-};
